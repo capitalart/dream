c127686ab55e7a779546bc4ed65c5dcce3247470
Robin Custance <dream@art.us-central1-f.c.art-narrator.internal>
2025-08-05 20:05:28 +0930

    🎉 Initial commit from full code stack

---
+++ .bash_history
@@ -0,0 +1,18 @@
+sudo whoami
+cd ~
+pwd
+git init
+git remote add origin git@github.com:capitalart/dream.git
+git branch -m main 
+git push -u origin main   
+su - dream
+cd ~
+touch testfile.txt
+nano ~/.ssh/config
+exit
+cat ~/.ssh/dream.pub
+exit
+mkdir -p ~/.ssh
+nano ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+chown -R dream:dream ~/.ssh
---
+++ .vscode-server/cli/servers/Stable-488a1f239235055e34e673291fb8d8c810886f81/server/bin/helpers/browser.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+ROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+
+APP_NAME="code"
+VERSION="1.102.3"
+COMMIT="488a1f239235055e34e673291fb8d8c810886f81"
+EXEC_NAME="code"
+CLI_SCRIPT="$ROOT/out/server-cli.js"
+"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
